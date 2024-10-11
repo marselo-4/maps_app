f4a4be4cdb04be9ffff6d8fb353ffe2dd8efdb76
marselo-4 <carlosmartinezguixa@gmail.com>
2024-10-11 13:01:15 +0200

    push

---
+++ lib/screens/gps_access_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:maps_app/blocs/blocs.dart';
+import 'package:maps_app/screens/map_screen.dart';
+
+class GpsAccessScreen extends StatelessWidget {
+  const GpsAccessScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: BlocBuilder<GpsBloc, GpsState>(
+        builder: (context, state) {
+          print('state: $state');
+
+          return !state.isGpsEnabled
+              ? const _EnableGpsMessage()
+              : const _AccessButton();
+        },
+      )),
+    );
+  }
+}
+
+class _AccessButton extends StatelessWidget {
+  const _AccessButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset("assets/img/error.png"),
+        const Text('Es necesario el acceso al GPS'),
+        const SizedBox(
+          height: 15,
+        ),
+        ElevatedButton(
+          onPressed: () {
+            // TODO
+            print("Boton presionado");
+          },
+          child: const Text(
+            "Solicitar acceso",
+            style: TextStyle(color: Colors.white),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class _EnableGpsMessage extends StatelessWidget {
+  const _EnableGpsMessage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset("assets/img/error.png"),
+        const Text(
+          'Debe de habilitar el GPS',
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.w300),
+        ),
+      ],
+    );
+  }
+}
