34554655ed9640fa41044e10f837915e67b317e1
marselo-4 <carlosmartinezguixa@gmail.com>
2024-10-25 10:06:34 +0200

    Permission Handlers

--- lib/blocs/gps/gps_state.dart
+++ lib/blocs/gps/gps_state.dart
@@ -7,6 +7,13 @@ class GpsState extends Equatable {
   const GpsState(
       {required this.isGpsEnabled, required this.isGpsPermissionGranted});
 
+  GpsState copyWith(
+          {bool? isGpsEnabled, bool? isGpsPermissionGranted}) =>
+      GpsState(
+          isGpsEnabled: isGpsEnabled ?? this.isGpsEnabled,
+          isGpsPermissionGranted:
+              isGpsPermissionGranted ?? this.isGpsPermissionGranted);
+
   @override
   List<Object> get props => [isGpsEnabled, isGpsPermissionGranted];
 
